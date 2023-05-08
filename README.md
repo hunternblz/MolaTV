89436cd7f034cae260a3100ecb783783774d9843
Muhammad Nabil <official.hunternblz@gmail.com>
2023-05-08 13:55:00 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<H1 align="center">
+Mola TV MPS Package
+</H1>
+
+<p align="center">
+  <br>
+  <img src="https://user-images.githubusercontent.com/34278783/236753585-0bd45537-6e97-4107-acdb-dfd656fbd8f8.png" width="640" title="Screenshot" alt="Screenshot">
+</p>
+
+## Termux
+
+* Termux v0.118 : <a href="https://f-droid.org/repo/com.termux_118.apk">DOWNLOAD</a>
+
+```terminal
+$ pkg update -y
+$ pkg upgrade -y
+$ pkg install git -y
+$ pkg install python -y
+$ git clone https://github.com/hunternblz/MolaTV
+$ cd MolaTV
+$ python run.py
+```
---
+++ run.py
@@ -0,0 +1,22 @@
+# Muhammad Nabil {HunterNblz}
+
+# Facebook  : https://www.facebook.com/HunterNblz.Real
+# Instagram : https://www.instagram.com/hunternblz
+# Github    : https://github.com/hunternblz
+# Telegram  : https://t.me/HunterNblz
+
+import os
+import subprocess
+
+if __name__ == "__main__":
+	os.system('clear')
+	try:
+		result = subprocess.run(["uname", "-m"], capture_output=True, text=True)
+		if result.returncode == 0:
+			architecture = result.stdout.strip()
+			if architecture == "aarch64":
+				__import__("HunterNblz").main()
+			else:
+				print("[!] Perangkat Harus 64 Bit [!]\n")
+	except Exception as e:
+		exit(str(e))
